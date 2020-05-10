c333312765f131ac3a9896543f716cdfe8db3cec
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-09 20:54:16 -0400

    Upload spring boot

---
+++ audio_video/src/test/java/com/audio_video/audio_video/AudioVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio_video.audio_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
